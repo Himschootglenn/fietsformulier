9449f34ba4b5572eaf854c883d5de0e36807e36b
hallejuja <55379099+hallejuja@users.noreply.github.com>
2019-10-14 15:22:49 +0200

    deel 3

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
 $(document).ready(function () {
-    
+    $('.stadsfietsDames').on('click', function () {
+        $('.maat43').hide().next().hide();
+        $('.maat50').show().next().show();
+        $('.maat57').show().next().show();
+        $('.maat61').hide().next().hide();
+        $('.geenMaat').hide().next().hide();
+    });
+
+    $('.stadsfietsHeren').on('click', function () {
+        $('.maat43').hide().next().hide();
+        $('.maat50').show().next().show();
+        $('.maat57').show().next().show();
+        $('.maat61').show().next().show();
+        $('.geenMaat').hide().next().hide();
+    });
+
+    $('.elec-fietsDames').on('click', function () {
+        $('.maat43').show().next().show();
+        $('.maat50').show().next().show();
+        $('.maat57').show().next().show();
+        $('.maat61').hide().next().hide();
+        $('.geenMaat').hide().next().hide();
+    });
+
+    $('.elec-fietsHeren').on('click', function () {
+        $('.maat43').hide().next().hide();
+        $('.maat50').show().next().show();
+        $('.maat57').show().next().show();
+        $('.maat61').show().next().show();
+        $('.geenMaat').hide().next().hide();
+    });
+
+    $('.vouwfiets').on('click', function () {
+        $('.maat43').hide().next().hide();
+        $('.maat50').hide().next().hide();
+        $('.maat57').hide().next().hide();
+        $('.maat61').hide().next().hide();
+        $('.geenMaat').show().next().show();
+    });
 });
